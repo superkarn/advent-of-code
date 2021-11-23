375a5327e474bed7c5fe9e1cef03ed69071c8b6a
Karn Ratana <karn@ratana.info>
2021-11-23 22:15:16 +0000

    Add 2015-03 getHousesCount2()

--- src/2015/03/AoC_2015_03.ts
+++ src/2015/03/AoC_2015_03.ts
@@ -42,14 +42,61 @@ namespace AoC {
             //console.log(`g  `, grid.length, grid);
             return grid.length;
         };
+        
+        getHousesCount2 = (list: string): number  => {
+            // The current position in the grid: x,y
+            let x: number[] = [0, 0];
+            let y: number[] = [0, 0];
+
+            // The grid, with starting position 0,0
+            // - right is positive x
+            // - top is positive y
+            let grid = [];
+            grid.push([0,0]);            
+            
+            // i is even = Santa
+            // i is odd = Robo-Santa
+            for (let i=0; i<list.length; i++) {
+                //console.log(`${i}: ${list[i]}`);
+
+                let travelerIndex = i % 2;
+
+                switch(list[i]) {
+                    case '^': // north
+                        y[travelerIndex]++;
+                        break;
+                        
+                    case '>': // east
+                        x[travelerIndex]++;
+                        break;
+                        
+                    case 'v': // south
+                        y[travelerIndex]--;
+                        break;
+                        
+                    case '<': // east
+                        x[travelerIndex]--;
+                        break;
+                }
+                
+                grid.push([x[travelerIndex], y[travelerIndex]]);
+                //console.log(`   `, grid);
+            }
+
+            // remove duplicates
+            grid = this.getDistinctItems(grid);
+
+            //console.log(`g  `, grid.length, grid);
+            return grid.length;
+        };
 
         getDistinctItems = (list: any[]): any[] => {
             // 1. map int[][] into string[]
             // 2. filter out duplicates
             // 3. map string[] back to int[][]
-            return list.map(item => `${item[0]}-${item[1]}`)
+            return list.map(item => `${item[0]}#${item[1]}`)
                         .filter((value, index, self) => self.indexOf(value) === index)
-                        .map(item => [parseInt( item.split('-')[0]), parseInt( item.split('-')[1])]);
+                        .map(item => [parseInt( item.split('#')[0]), parseInt( item.split('#')[1])]);
         };
     }
 }
@@ -59,3 +106,4 @@ input = fs.readFileSync('../../../2015/03/data.txt', {encoding:'utf8'}).toString
 
 let aoc_2015_03: AoC.AoC_2015_03 = new AoC.AoC_2015_03();
 console.log(aoc_2015_03.getHousesCount(input)); // 2592
+console.log(aoc_2015_03.getHousesCount2(input)); // 2360
