969ee9a05b418343f45d2238fb6e0f70cbd3c820
Karn Ratana <karn@ratana.info>
2021-11-24 04:38:40 +0000

    Add 2015-04 part 2

--- src/2015/04/AoC_2015_04.ts
+++ src/2015/04/AoC_2015_04.ts
@@ -2,15 +2,16 @@ namespace AoC {
     export class AoC_2015_04 {
         CryptoJS = require('crypto-js');
 
-        mine = (key: string): number  => {
+        mine = (key: string, zeros: number): number  => {
             let i: number = 0;
+            let startingZeros = ''.padStart(zeros,'0');
 
             while (true) {
                 let hash = this.CryptoJS.MD5(`${key}${i}`).toString();
                 //console.log(`${i}: ${hash}`);
 
-                // found a hash that starts with 00000
-                if (hash.substring(0,5) === "00000") {
+                // found the first solution
+                if (hash.substring(0,zeros) === startingZeros) {
                     break;
                 }
 
@@ -26,4 +27,5 @@ fs = require('fs');
 input = fs.readFileSync('../../../2015/04/data.txt', {encoding:'utf8'}).toString();
 
 let AoC_2015_04: AoC.AoC_2015_04 = new AoC.AoC_2015_04();
-console.log(AoC_2015_04.mine(input)); // 
+console.log(AoC_2015_04.mine(input, 5)); // 254575
+console.log(AoC_2015_04.mine(input, 6)); // 1038736
