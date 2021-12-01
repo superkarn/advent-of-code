200a5e60e0e2a62ac94f54fd7c82690b94f02a5e
Karn Ratana <karn@ratana.info>
2021-12-01 13:01:46 +0000

    Add 2021-01 part 2

--- src/2021/01/AoC_2021_01.ts
+++ src/2021/01/AoC_2021_01.ts
@@ -22,6 +22,27 @@ namespace AoC._2021_01 {
 
             return result;
         };
+
+        countSumIncrements = (list: number[]): number => {
+            let result: number = 0;
+
+            for (let i=3; i<list.length; i++) {
+                // // We want to compare the currentSum to previousSum
+                // let previousSum = list[i-3] + list[i-2] + list[i-1];
+                // let currentSum = list[i-2] + list[i-1] + list[i];
+                // if (currentSum > previousSum) {
+                //     result++;
+                // }
+
+                // Since list[i-2] and list[i-1] are in both, they cancel out
+                // and we only have to check list[i] and list[i-3]
+                if (list[i] > list[i-3]) {
+                    result++;
+                }
+            }
+
+            return result;
+        };
     }
 }
 
@@ -29,4 +50,5 @@ fs = require('fs');
 input = fs.readFileSync('../../../2021/01/data.txt', {encoding:'utf8'}).toString();
 
 let AoC_2021_01: AoC._2021_01.Main = new AoC._2021_01.Main();
-console.log(AoC_2021_01.countIncrements(AoC_2021_01.parseInput(input))); // 
+console.log(AoC_2021_01.countIncrements(AoC_2021_01.parseInput(input))); // 1521
+console.log(AoC_2021_01.countSumIncrements(AoC_2021_01.parseInput(input))); // 1543
