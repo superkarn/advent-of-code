69809cb91d25da366cfc4140cdff8beee97c71a4
Karn Ratana <karn@ratana.info>
2021-12-02 19:55:24 +0000

    Refactor 2021-01

--- src/2021/01/AoC_2021_01.ts
+++ src/2021/01/AoC_2021_01.ts
@@ -11,32 +11,13 @@ namespace AoC._2021_01 {
             return result;
         }
 
-        countIncrements = (list: number[]): number => {
+        countTerrainIncrements = (list: number[], range: number = 1): number => {
             let result: number = 0;
 
-            for (let i=1; i<list.length; i++) {
-                if (list[i] > list[i-1]) {
-                    result++;
-                }
-            }
-
-            return result;
-        };
-
-        countSumIncrements = (list: number[]): number => {
-            let result: number = 0;
-
-            for (let i=3; i<list.length; i++) {
-                // // We want to compare the currentSum to previousSum
-                // let previousSum = list[i-3] + list[i-2] + list[i-1];
-                // let currentSum = list[i-2] + list[i-1] + list[i];
-                // if (currentSum > previousSum) {
-                //     result++;
-                // }
-
-                // Since list[i-2] and list[i-1] are in both, they cancel out
-                // and we only have to check list[i] and list[i-3]
-                if (list[i] > list[i-3]) {
+            for (let i=range; i<list.length; i++) {
+                // Since the middle terrains are in both, they cancel out
+                // and we only have to check first and last
+                if (list[i] > list[i-range]) {
                     result++;
                 }
             }
@@ -50,5 +31,5 @@ fs = require('fs');
 input = fs.readFileSync('../../../2021/01/data.txt', {encoding:'utf8'}).toString();
 
 let AoC_2021_01: AoC._2021_01.Main = new AoC._2021_01.Main();
-console.log(AoC_2021_01.countIncrements(AoC_2021_01.parseInput(input))); // 1521
-console.log(AoC_2021_01.countSumIncrements(AoC_2021_01.parseInput(input))); // 1543
+console.log(AoC_2021_01.countTerrainIncrements(AoC_2021_01.parseInput(input), 1)); // 1521
+console.log(AoC_2021_01.countTerrainIncrements(AoC_2021_01.parseInput(input), 3)); // 1543
