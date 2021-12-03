79183efdd4566666c87baf74fad85118211586e5
Karn Ratana <karn@ratana.info>
2021-12-03 14:23:50 +0000

    Add part 2

--- src/2021/03/AoC_2021_03.ts
+++ src/2021/03/AoC_2021_03.ts
@@ -41,9 +41,89 @@ namespace AoC._2021_03 {
             return this.convertArrayToDecimal(gammaArray) * this.convertArrayToDecimal(epsilonArray);
         };
 
+        getLifeSupportRating = (list: string[]): number => {
+            return this.getOxygenGeneratorRating(list) * this.getCO2GeneratorRating(list);
+        };
+
+        private getOxygenGeneratorRating = (list: string[]): number => {
+            let filteredList: string[] = list;
+            let j: number = 0;
+
+            // loop through each digit
+            while (filteredList.length > 1 && j < filteredList[0].length) {
+                let count0 = 0;
+                let count1 = 0;
+
+                // loop through the list
+                for (let i=0; i<filteredList.length; i++) {
+                    switch(filteredList[i][j]) {
+                        case "0":
+                            count0++;
+                            break;
+
+                        case "1":
+                            count1++;
+                            break;
+                    }
+                }
+
+                // Keep the higher count.  If equal, keep 1.
+                filteredList = filteredList.filter((x:string) => x[j] == (count1 >= count0 ? "1" : "0"));
+
+                j++;
+            }
+
+            // There should only be one number left
+            if (filteredList.length > 1) {
+                throw new RangeError(`Too many numbers left: ${filteredList.length}`);
+            }
+
+            return parseInt(filteredList[0], 2);
+        };
+
+        
+        private getCO2GeneratorRating = (list: string[]): number => {
+            let filteredList: string[] = list;
+            let j: number = 0;
+
+            // loop through each digit
+            while (filteredList.length > 1 && j < filteredList[0].length) {
+                let count0 = 0;
+                let count1 = 0;
+
+                // loop through the list
+                for (let i=0; i<filteredList.length; i++) {
+                    switch(filteredList[i][j]) {
+                        case "0":
+                            count0++;
+                            break;
+
+                        case "1":
+                            count1++;
+                            break;
+                    }
+                }
+
+                // Keep the lower count.  If equal, keep 0.
+                filteredList = filteredList.filter((x:string) => x[j] == (count1 < count0 ? "1" : "0"));
+
+                j++;
+            }
+
+            // There should only be one number left
+            if (filteredList.length > 1) {
+                throw new RangeError(`Too many numbers left: ${filteredList.length}`);
+            }
+
+            return parseInt(filteredList[0], 2);
+        };
+
         private convertArrayToDecimal = (value: number[]): number => {
+            if (!value || value.length == 0) {
+                return 0;
+            }
+
             let binaryString: string = value.reduce((acc, cur) => acc + cur.toString(), "");
-            console.log(`binaryString: ${binaryString}`);
             return parseInt(binaryString, 2);
         };
     }
@@ -54,3 +134,4 @@ input = fs.readFileSync('../../../2021/03/data.txt', {encoding:'utf8'}).toString
 
 let AoC_2021_03: AoC._2021_03.Main = new AoC._2021_03.Main();
 console.log(AoC_2021_03.getPowerConsumption(AoC_2021_03.parseInput(input))); // 841526
+console.log(AoC_2021_03.getLifeSupportRating(AoC_2021_03.parseInput(input))); // 4790390
