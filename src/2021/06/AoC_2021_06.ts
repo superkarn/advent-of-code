2325fd97cd040b82511296e8a7dfb58470251e60
Karn Ratana <karn@ratana.info>
2021-12-06 15:23:36 +0000

    Attempt to optimize the code

--- src/2021/06/AoC_2021_06.ts
+++ src/2021/06/AoC_2021_06.ts
@@ -1,15 +1,13 @@
 namespace AoC._2021_06 {
     export class Main {
-        poolOfFish: Fish[];
+        // The rough size of each pool of Fish
+        static readonly POOL_SIZE: number = 100000;
 
-        constructor() {}
+        poolsOfFish: Fish[][];
 
-        toString = (): string => {
-            let result: string = '';
-
-
-            return result;
-        };
+        constructor() {
+            this.poolsOfFish = [];
+        }
 
         parseInput = (input: string): void => {
             let list = input
@@ -24,27 +22,63 @@ namespace AoC._2021_06 {
                     }
                 })
 
-            this.poolOfFish = list;
+            this.poolsOfFish[0] = list;
         };
 
         simulateDays = (days: number): number => {
+            let currentPool = 0;
             for (let i=0; i<days; i++) {
-                //console.log(`  day ${i}: ${this.poolOfFish.length} => ${this.poolOfFish}`);
+                //console.log(`${i}: ${this.poolsOfFish[currentPool].length}`);
+                //console.log(`  day ${i}: ${this.poolsOfFish[currentPool].length} => ${this.poolsOfFish[currentPool]}`);
 
-                let offsprings: Fish[] = new Array();
+                let offsprings: Fish[] = [];
 
-                for (let f=0; f<this.poolOfFish.length; f++) {
-                    let hasNewOffspring: boolean = this.poolOfFish[f].grow();
+                for (let f1=0; f1<this.poolsOfFish.length; f1++) {
+                    for (let f2=0; f2<this.poolsOfFish[f1].length; f2++) {
+                        let hasNewOffspring: boolean = this.poolsOfFish[f1][f2].grow();
 
-                    if (hasNewOffspring) {
-                        offsprings.push(new Fish());
+                        if (hasNewOffspring) {
+                            offsprings.push(new Fish());
+                        }
                     }
                 }
 
-                this.poolOfFish.push(...offsprings);
+                // If the pool is already larger than max, go to the next pool
+                if (this.poolsOfFish[currentPool].length > Main.POOL_SIZE) {
+                    currentPool++;
+                    console.log(`--adding a new pool: ${currentPool}`);
+
+                    this.poolsOfFish[currentPool] = [];
+                }
+
+                console.log(`    offspring size: ${offsprings.length}`);
+                // Add the offspring to the pool
+                // - Using array1.merge(array2) is very slow, and the heap can run out of memory
+                //this.poolsOfFish[currentPool] = this.poolsOfFish[currentPool].concat(offsprings);
+                //
+                // - Using array1.push(...array2) is fast, but can run out of stack size when array2 is too large
+                //this.poolsOfFish[currentPool].push(...offsprings);
+                //
+                // - So we're using a custom merge instead
+                while (offsprings.length > 0) {
+                    //console.log(`      merging offspring size: ${offsprings.length}`);
+                    this.poolsOfFish[currentPool].push(...(offsprings.splice(0,10000)));
+                }
             }
 
-            return this.poolOfFish.length;
+            // this.poolsOfFish.map((value, index) => {
+            //     console.log(`-- pool[${index}] size: ${value.length}`);
+            // });
+            return this.poolsOfFish.reduce((acc, cur) => acc+=cur.length, 0);
+        };
+
+        // Add arr2 into arr1 in batches
+        private mergeArrays = (arr1: Fish[], arr2: Fish[]): void => {
+            let arraySize = Main.POOL_SIZE;
+
+            while (arr2.length > 0) {
+                arr1.push(...(arr2.splice(0, arraySize)));
+            }
         };
     }
 
@@ -75,9 +109,11 @@ namespace AoC._2021_06 {
     }
 }
 
+
 fs = require('fs');
 input = fs.readFileSync('../../../2021/06/data.txt', {encoding:'utf8'}).toString();
 
 let AoC_2021_06: AoC._2021_06.Main = new AoC._2021_06.Main();
 AoC_2021_06.parseInput(input);
-console.log(AoC_2021_06.simulateDays(80)); // 346063
+//console.log(AoC_2021_06.simulateDays(80)); // 346063
+console.log(AoC_2021_06.simulateDays(256)); // 
