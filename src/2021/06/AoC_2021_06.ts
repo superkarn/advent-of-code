b0d3a62ac610683ad1e7dbdd4c28192ae1ccf405
Karn Ratana <karn@ratana.info>
2021-12-07 04:03:01 +0000

    TEMP

--- src/2021/06/AoC_2021_06.ts
+++ src/2021/06/AoC_2021_06.ts
@@ -12,9 +12,10 @@ namespace AoC._2021_06 {
         parseInput = (input: string): void => {
             let list = input
                 .split(',')
+                .sort((a: string, b: string) => parseInt(a) - parseInt(b))
                 .map((value: string) => {
                     try {
-                        return new Fish(parseInt(value));
+                        return new Fish(value);
                     } catch (error) {
                         console.log(`error: ${error}`);
                         console.log(`value: ${value}`);
@@ -26,11 +27,13 @@ namespace AoC._2021_06 {
         };
 
         simulateDays = (days: number): number => {
+            let cutoff = 200;
+
             let maxPoolSize = 0;
+            let offsprings: Fish[] = [];
+
             for (let i=0; i<days; i++) {
                 console.log(`  new day[${i}]. ${this.poolsOfFish.length} pools`);
-
-                let offsprings: Fish[] = [];
 
                 for (let f1=0; f1<this.poolsOfFish.length; f1++) {
                     //process.stdout.write(`.`);
@@ -43,16 +46,23 @@ namespace AoC._2021_06 {
                             offsprings.push(new Fish());
                         }
                     }
+
+                    if (this.poolsOfFish.length >= cutoff && offsprings.length > 0) {
+                        process.stdout.write(`.`);
+                        //console.log(`  day[${i}], pool[${f1}], ${offsprings.length} new offsprings`);
+                       
+                        while (offsprings.length > 0) {
+                            maxPoolSize = Math.max(maxPoolSize, offsprings.length);
+                    
+                            //console.log('Memory    : ', process.memoryUsage().heapUsed);
+                            //console.log(`  day[${i}], pool[${f1}], creating a new pool for total of ${this.poolsOfFish.length}    ${maxPoolSize}`);
+                            this.poolsOfFish.push(offsprings.splice(0,Main.POOL_SIZE));
+                        }
+                    
+                        offsprings = [];
+                    }
                 }
                 //console.log(`;`);
-
-                // // If the pool is already larger than max, go to the next pool
-                // if (this.poolsOfFish[currentPool].length > Main.POOL_SIZE) {
-                //     currentPool++;
-                //     console.log(`--adding a new pool: ${currentPool}`);
-
-                //     this.poolsOfFish[currentPool] = [];
-                // }
 
                 // Add the offspring to the pool
                 // 1- Using array1.merge(array2) is very slow, and the heap can run out of memory
@@ -68,16 +78,16 @@ namespace AoC._2021_06 {
                 // }
                 //
                 // 4- always add offsprings into a new pool by themselves
-                if (offsprings.length > 0) {
+                if (this.poolsOfFish.length < cutoff && offsprings.length > 0) {
+                    process.stdout.write(`*`);
                     console.log(`  day[${i}], ${offsprings.length} new offsprings`);
-                    
+                   
                     while (offsprings.length > 0) {
                         maxPoolSize = Math.max(maxPoolSize, offsprings.length);
-
-                        //console.log('Memory    : ', process.memoryUsage().heapUsed);
-                        console.log(`  day[${i}], creating a new pool for total of ${this.poolsOfFish.length}    ${maxPoolSize}`);
+                
                         this.poolsOfFish.push(offsprings.splice(0,Main.POOL_SIZE));
                     }
+                    offsprings = [];
                 }
             }
 
@@ -86,15 +96,40 @@ namespace AoC._2021_06 {
             // });
             return this.poolsOfFish.reduce((acc, cur) => acc+=cur.length, 0);
         };
+
+        simulateDays2 = (days: number): number => {
+            return this.growByMath(parseInt(this.poolsOfFish[0][0].age), days);
+        };
+
+        
+        growByMath = (age: number, days: number): number => {
+            if (days < age) {
+                console.log(`age:${age}, days:${days} returning 0`);
+                return 0;
+            }
+
+            let result: number = 0;
+            let offsprings: number = Math.max(0, Math.floor((days - age)/7) + 1);
+            console.log(`age:${age}, days:${days} -> ${offsprings}`);
+
+            result += offsprings;
+            for (let i=0; i<offsprings; i++) {
+                let offspringAge = Math.max(0, age-1);
+                result += this.growByMath(offspringAge, days-1);
+            }
+
+
+            return result;
+        };
     }
 
     class Fish {
         static readonly DEFAULT_AGE: number = 6; // 7 days in a 0th based counting system
         static readonly MATURITY_OFFSET: number = 2;
 
-        age: number;
+        age: string;
 
-        constructor(age: number = (Fish.DEFAULT_AGE+Fish.MATURITY_OFFSET)) {
+        constructor(age: string = ''+ (Fish.DEFAULT_AGE+Fish.MATURITY_OFFSET)) {
             this.age = age;
         }
 
@@ -104,11 +139,11 @@ namespace AoC._2021_06 {
 
         // Returns true, if it produces an offspring
         grow = (): boolean => {
-            if (this.age == 0) {
-                this.age = Fish.DEFAULT_AGE;
+            if (this.age == '0') {
+                this.age = ''+ Fish.DEFAULT_AGE;
                 return true
             } else {
-                this.age--;
+                this.age = '' + (parseInt(this.age)-1);
                 return false;
             }
         };
@@ -122,4 +157,4 @@ input = fs.readFileSync('../../../2021/06/data.txt', {encoding:'utf8'}).toString
 let AoC_2021_06: AoC._2021_06.Main = new AoC._2021_06.Main();
 AoC_2021_06.parseInput(input);
 //console.log(AoC_2021_06.simulateDays(80)); // 346063
-console.log(AoC_2021_06.simulateDays(256)); // 
+console.log(AoC_2021_06.simulateDays2(4)); // 
