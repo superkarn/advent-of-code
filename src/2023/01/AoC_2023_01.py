8da8271ae4c44122e80e9d5c6aebf82f99038d7f
Karn Ratana <karn@ratana.info>
2023-12-05 23:02:50 -0500

    Refactored some code

--- src/2023/01/AoC_2023_01.py
+++ src/2023/01/AoC_2023_01.py
@@ -1,40 +1,36 @@
 
+DIGITS = {
+    "0":"zero", 
+    "1":"one", 
+    "2":"two", 
+    "3":"three", 
+    "4":"four", 
+    "5":"five", 
+    "6":"six", 
+    "7":"seven", 
+    "8":"eight", 
+    "9":"nine"
+}
+
 def get_lines_from_file_name(file_name):
     data = open(file_name, 'r')
     return data.readlines()
 
+def get_digit_from_line(line, index, character):
+    # Check for numeral numbers, returns matching numeral number
+    if character in DIGITS:
+        return character
+    
+    # Check for spelled out numbers, returns corresponding numeral number
+    for number, string in DIGITS.items():
+        if string == line[index: index+len(string)]:
+            return str(number)
+
 def get_first_number(line):
     for i, a in enumerate(line):
-        match a:
-            case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
-                return a
-            case "z":
-                if "zero" == line[i: i+len("zero")]:
-                    return "0"
-            case "o":
-                if "one" == line[i: i+len("one")]:
-                    return "1"
-            case "t":
-                if "two" == line[i: i+len("two")]:
-                    return "2"
-                if "three" == line[i: i+len("three")]:
-                    return "3"
-            case "f":
-                if "four" == line[i: i+len("four")]:
-                    return "4"
-                if "five" == line[i: i+len("five")]:
-                    return "5"
-            case "s":
-                if "six" == line[i: i+len("six")]:
-                    return "6"
-                if "seven" == line[i: i+len("seven")]:
-                    return "7"
-            case "e":
-                if "eight" == line[i: i+len("eight")]:
-                    return "8"
-            case "n":
-                if "nine" == line[i: i+len("nine")]:
-                    return "9"
+        result = get_digit_from_line(line, i, a)
+        if result is not None:
+            return result
 
 def get_last_number(line):
     # line = the original line
@@ -45,36 +41,10 @@ def get_last_number(line):
     line_reversed = ''.join(reversed(line))
     for i_reversed, a in enumerate(line_reversed):
         i = len(line) - i_reversed - 1;
-        match a:
-            case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
-                return a
-            case "z":
-                if "zero" == line[i: i+len("zero")]:
-                    return "0"
-            case "o":
-                if "one" == line[i: i+len("one")]:
-                    return "1"
-            case "t":
-                if "two" == line[i: i+len("two")]:
-                    return "2"
-                if "three" == line[i: i+len("three")]:
-                    return "3"
-            case "f":
-                if "four" == line[i: i+len("four")]:
-                    return "4"
-                if "five" == line[i: i+len("five")]:
-                    return "5"
-            case "s":
-                if "six" == line[i: i+len("six")]:
-                    return "6"
-                if "seven" == line[i: i+len("seven")]:
-                    return "7"
-            case "e":
-                if "eight" == line[i: i+len("eight")]:
-                    return "8"
-            case "n":
-                if "nine" == line[i: i+len("nine")]:
-                    return "9"
+        
+        result = get_digit_from_line(line, i, a)
+        if result is not None:
+            return result
 
 
 lines = get_lines_from_file_name("./data.txt")
@@ -85,7 +55,7 @@ for line in lines:
     n2 = get_last_number(line)
     
     result += int(n1+n2)
-
+    #print(f"  {result} <- {int(n1+n2)} <- {n1} + {n2} <- {line}")
 
 print(f"{result}")
 
