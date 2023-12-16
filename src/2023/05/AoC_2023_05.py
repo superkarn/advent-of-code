faa7670aa6a4fb5954d88ec885f8155442f39ed5
Karn Ratana <karn@ratana.info>
2023-12-16 16:12:33 -0500

    2023-05 part 1

--- src/2023/05/AoC_2023_05.py
+++ src/2023/05/AoC_2023_05.py
@@ -19,7 +19,7 @@ def load_data():
 
     # Parse the mapping
     current_map_type = MapType.SEED
-    current_map = []
+    current_map = Map()
     for line in lines:
         # Skip the seed line
         if "seeds: " in line:
@@ -33,18 +33,22 @@ def load_data():
         if " map:" in line:
             result.maps.append(current_map)
             current_map_type = MapType(int(current_map_type)+1)
-            current_map = []
+            current_map = Map()
             continue
 
         # Parse the ranges
         line_info = line.split(':')
-        map_line = Map(*list(map(lambda x: int(x),
+        map_line = Range(*list(map(lambda x: int(x),
                             line.split(' ')
         )))
-        current_map.append(map_line)
+        current_map.ranges.append(map_line)
 
     # Append the last map
     result.maps.append(current_map)
+
+    # Get rid of the first, empty map
+    result.maps.pop(0)
+
 
     return result
 
@@ -56,6 +60,23 @@ class AoC_05:
     def __str__(self) -> str:
         return f"\n  seeds: {self.seeds}\n  maps: {self.maps}"
     
+    def get_lowest_location(self) -> int:
+        locations = []
+        for i, seed in enumerate(self.seeds):
+            location = self.maps[MapType.HUMIDITY].get_destination(
+                self.maps[MapType.TEMPERATURE].get_destination(
+                self.maps[MapType.LIGHT].get_destination(
+                self.maps[MapType.WATER].get_destination(
+                self.maps[MapType.FERTILIZER].get_destination(
+                self.maps[MapType.SOIL].get_destination(
+                self.maps[MapType.SEED].get_destination(seed)
+            ))))))
+            
+            #print(f"seed[{i}]: {data.seeds[i]} -> {location}")
+            locations.append(location)
+        
+        return min(locations)
+
 class MapType(IntEnum):
     SEED = 0
     SOIL = 1
@@ -64,10 +85,9 @@ class MapType(IntEnum):
     LIGHT = 4
     TEMPERATURE = 5
     HUMIDITY = 6
-    LOCATION = 7
+    LOCATION = 7 # This map doesn't exist, but as the last location
 
-# TODO Expand this to cover multiple maps
-class Map:
+class Range:
     
     def __init__(self, dest, src, len) -> None:
         self.source_start = src
@@ -88,8 +108,31 @@ class Map:
         if self.source_start <= src and src < self.source_start+self.range_length:
             result = self.destination_start + (src - self.source_start)
 
-        print(f"  mapped {src} -> {result}")
+        #print(f"  mapped {src} -> {result}")
         return result
+    
+class Map:
+    ranges = []
+
+    def __init__(self) -> None:
+        self.ranges = []
+    
+    def __str__(self) -> str:
+        return f"Map.ranges: {self.ranges}\n"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def get_destination(self, src) -> int:
+        result = src
+        for range in self.ranges:
+            x = range.get_destination(src)
+            if x != src:
+                result = x
+                break
+
+        return result
+
 ##### Run the code
 
 # Load the game data
@@ -98,10 +141,8 @@ data = load_data()
 end_time = datetime.datetime.now()
 print(f"Loading data took {(end_time-start_time).microseconds} microseconds")
 
-print(f"data: {data}")
-
 # Calculate the data for part 1
 start_time = datetime.datetime.now()
-result1 = 0
+result1 = data.get_lowest_location()
 end_time = datetime.datetime.now()
-print(f"Part 1: {result1} ({(end_time-start_time).microseconds} microseconds)")
+print(f"Part 1: {result1} (Took {(end_time-start_time).microseconds} microseconds)")
